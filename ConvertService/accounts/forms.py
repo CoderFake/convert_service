f17f49c56e8b2a94691c9101606cd743f95572d5
hoangdieu <hoangdieu22022002@gmail.com>
2024-12-09 14:50:01 +0700

    update: remove login form

--- ConvertService/accounts/forms.py
+++ ConvertService/accounts/forms.py
@@ -1,23 +1,26 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
+#
+#
+# class LoginForm(forms.Form):
+#     username = forms.CharField(
+#         widget=forms.TextInput(
+#             attrs={
+#                 "placeholder": "メールアドレスを入力してください",
+#                 "class": "form-control"
+#             }
+#         ))
+#     password = forms.CharField(
+#         widget=forms.PasswordInput(
+#             attrs={
+#                 "placeholder": "パスワードを入力してください",
+#                 "class": "form-control"
+#             }
+#         ))
+#     remember_username = forms.BooleanField(required=False, widget=forms.CheckboxInput())
+#     remember_password = forms.BooleanField(required=False, widget=forms.CheckboxInput())
 
-
-class LoginForm(forms.Form):
-    username = forms.CharField(
-        widget=forms.TextInput(
-            attrs={
-                "placeholder": "メールアドレスを入力してください",
-                "class": "form-control"
-            }
-        ))
-    password = forms.CharField(
-        widget=forms.PasswordInput(
-            attrs={
-                "placeholder": "パスワードを入力してください",
-                "class": "form-control"
-            }
-        ))
 
 
 class SignUpForm(UserCreationForm):
