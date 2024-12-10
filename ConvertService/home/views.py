e48c70a4cef147dc243f0efcd5eeadacac4f25fc
hoangdieu <hoangdieu22022002@gmail.com>
2024-12-10 17:35:12 +0700

    update: add view for process file

--- ConvertService/home/views.py
+++ ConvertService/home/views.py
@@ -2,4 +2,14 @@ from django.shortcuts import render
 
 
 def home(request):
+    if request.user.is_authenticated:
+        tab = request.GET.get("tab", "upload-file")
+        if tab not in ["upload-file", "process-file"]:
+            tab = "upload-file"
+        context = {"tab": tab}
+
+        if tab == "process-file":
+            pass
+        return render(request, 'web/home/index.html', context)
     return render(request, 'web/home/index.html')
+
