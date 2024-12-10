fb313613b226901a6f97d76ddd1e0d0a8273b3e7
hoangdieu <hoangdieu22022002@gmail.com>
2024-12-10 11:48:04 +0700

    update: create mysql

--- db/initdb.d/create-db.sh
+++ db/initdb.d/create-db.sh
@@ -2,10 +2,10 @@
 set -e
 
 until mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "SELECT 1"; do
-  echo "Waiting for MariaDB..."
+  echo "Waiting for MySQL..."
   sleep 5
 done
 
-mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS ${DB_NAME} CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
+mysql -u root -p"${MYSQL_ROOT_PASSWORD}" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\` CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
 
-echo "Database ${DB_NAME} created successfully."
+echo "Database ${MYSQL_DATABASE} created successfully."
