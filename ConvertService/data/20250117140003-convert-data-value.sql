099192d395bde0becd0037f71230c7e1c198e88e
hoangdieu <hoangdieu22022002@gmail.com>
2025-01-21 15:39:58 +0700

    update: fix insert id into db

---
+++ ConvertService/data/20250117140003-convert-data-value.sql
@@ -0,0 +1,4 @@
+INSERT INTO convert_data_value (data_value_before, data_value_after, convert_rule_id, tenant_id) VALUES ('【B.変更】　　　健康診断　予約のお願い', '予約　さんぷる１', 8, 1),
+('', '固定', 8, 1);
+
+-- あなたはここにデフォルトの変換データを追加してください
