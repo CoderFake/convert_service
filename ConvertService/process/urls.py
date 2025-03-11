6c06dd971757c9cb3742868a570592afdcd1e0eb
hoangdieu <hoangdieu22022002@gmail.com>
2025-03-11 17:19:38 +0700

    update: chang urls use as_view()

--- ConvertService/process/urls.py
+++ ConvertService/process/urls.py
@@ -2,12 +2,11 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('upload-file/', views.upload_file, name='upload_file'),
-    path('delete-file/', views.delete_file, name='delete_file'),
-    path('process-files/', views.process_files, name='process_files'),
-    path('process-headers/', views.process_headers, name='process_headers'),
-    path('file-format/', views.format_data_processing, name='format_data_processing'),
-    path('download-zip/<str:zip_key>/', views.download_zip, name='download_zip'),
-    path('download/<str:download_type>/', views.download, name='download'),
-
+    path('upload-file/', views.UploadFileView.as_view(), name='upload_file'),
+    path('delete-file/', views.DeleteFileView.as_view(), name='delete_file'),
+    path('process-files/', views.ProcessFilesView.as_view(), name='process_files'),
+    path('process-headers/', views.ProcessHeadersView.as_view(), name='process_headers'),
+    path('file-format/', views.FormatDataProcessingView.as_view(), name='format_data_processing'),
+    path('download-zip/<str:zip_key>/', views.DownloadZipView.as_view(), name='download_zip'),
+    path('download/<str:download_type>/', views.DownloadView.as_view(), name='download'),
 ]
