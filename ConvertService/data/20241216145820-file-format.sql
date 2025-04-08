f1642b5b6c12a08c1b62b5af57192e3c1fa3924f
coderfake <hoangdieu22022002@gmail.com>
2025-04-08 09:09:53 +0700

    update: update sqls

--- ConvertService/data/20241216145820-file-format.sql
+++ ConvertService/data/20241216145820-file-format.sql
@@ -10,10 +10,10 @@ VALUES ('CSV_C_SJIS', 'CSVファイル（カンマ区切り）SJIS', NOW(), NOW(
 
 INSERT INTO data_format (tenant_id, data_format_id, data_format_name, file_format_id, created_at, updated_at)
 VALUES ((SELECT id FROM tenant WHERE tenant_id = 'T_001'), 'DF_001', '健診システム取込用データ',
-        (SELECT id FROM file_format WHERE file_format_id = 'CSV_C_SJIS'), NOW(), NOW()),
+        (SELECT id FROM file_format WHERE file_format_id = 'CSV_C_UTF-8'), NOW(), NOW()),
        ((SELECT id FROM tenant WHERE tenant_id = 'T_001'), 'DF_002', '予約代行業者Aの予約データ',
         (SELECT id FROM file_format WHERE file_format_id = 'JSON'), NOW(), NOW()),
        ((SELECT id FROM tenant WHERE tenant_id = 'T_001'), 'DF_003', '予約代行業者Bの予約データ',
-        (SELECT id FROM file_format WHERE file_format_id = 'CSV_C_UTF-8'), NOW(), NOW()),
+        (SELECT id FROM file_format WHERE file_format_id = 'CSV_C_SJIS'), NOW(), NOW()),
        ((SELECT id FROM tenant WHERE tenant_id = 'T_001'), 'DF_004', '予約代行業者Cの予約データ',
         (SELECT id FROM file_format WHERE file_format_id = 'EXCEL'), NOW(), NOW());
